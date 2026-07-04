56faeaa8a9efd0fc064d53c75e945779549225de
Lefteris <61457925+masterLef@users.noreply.github.com>
2026-07-04 15:35:39 +0300

    Configure custom domain theranodeapp.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Lefteris86.github.io',
-  base: '/moodLogger_webpage',
+  site: 'https://www.theranodeapp.com'
 });
